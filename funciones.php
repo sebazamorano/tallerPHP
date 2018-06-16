e6bc731f87bd302c5e990983e0e8e2f669d11497
Sebastian Zamorano <sebazamoranoj@gmail.com>
2018-06-16 09:22:00 -0400

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -21,8 +21,6 @@ function saludo(string &$nombre, string $apellido = null): string {
 $nombre = "sebastian";
 //echo saludo($nombre, "Zamorano");
 
-
-
 function cambiaColor(&$color) {
     $color = "amarillo";
 }
@@ -40,3 +38,40 @@ $a = function ($nombre) {
 //echo $a("sebastian");
 
 //var_dump(suma(2, 2));
+
+//Ordenamiento por seleccion
+$numero = ["8","7","1","9","2","6","3","4","5","10","8","7","1","9","2","6","3","4","5","10"];
+$k = 0;
+$i = 0;
+$j=0;
+for($i = 0; $i < sizeof($numero) - 1; $i++) {
+    for ($k = $i, $j = $i + 1; $j < sizeof($numero); $j++) {
+        if ($numero[$j] < $numero[$k]) {
+            $tmp = $numero[$i];
+            $numero[$k] = $numero[$j];
+            $numero[$j] = $tmp;
+        }
+    }
+}
+
+// Ordenamiento Burbuja
+
+$numero = ["8","7","1","9","2","6","3","4","5","10"];
+
+for ($i = 1; $i < sizeof($numero); $i++) {
+    for ($j = 0; $j < sizeof($numero) - $i; $j++) {
+        if ($numero[$j] > $numero[$j + 1]) {
+            $tmp = $numero[$j + 1];
+            $numero[$j + 1] = $numero[$j];
+            $numero[$j] = $tmp;
+        }
+    }
+}
+
+$fecha = new DateTime('1988-01-09');
+$time = new DateTime();
+//print_r($time->diff($fecha));
+
+for ($i = 1, $j = $i + 1; $i <= 20; $i ++) {
+    echo $i + $j;
+}
